b264cc19390ca8a4cb3543c00e71589475777fb0
supriya karmakar <140977487+09raja@users.noreply.github.com>
2024-06-03 10:45:11 +0000

    change

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,12 @@
+let Name ="himadri"
+
+let age = 18
+let isLoggedIn = false
+let State;
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
