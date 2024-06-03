2d6673a0a18f2f0276da3591171fb58d65c8bd21
supriya karmakar <140977487+09raja@users.noreply.github.com>
2024-06-03 10:21:03 +0000

    changes

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,5 +2,9 @@ const accountId =9976776
 let accountName ="supriya"
 var accountEmail ="karmakar98@gmail.com"
 accountCity ="joyrambati"
+let accountState;
 console.log(accountId);
-console.table([accountEmail,accountId,accountName,accountCity])
+console.table([accountEmail,accountId,accountName,accountCity,accountState])
+/*
+prefre not use var because issue of block scope and fuctional scope
+*/
