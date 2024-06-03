2a77856363be5b430bd933fd136f4843d30bc745
supriya karmakar <140977487+09raja@users.noreply.github.com>
2024-06-03 10:12:37 +0000

    change

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,6 @@
+const accountId =9976776
+let accountName ="supriya"
+var accountEmail ="karmakar98@gmail.com"
+accountCity ="joyrambati"
+console.log(accountId);
+console.table([accountEmail,accountId,accountName,accountCity])
