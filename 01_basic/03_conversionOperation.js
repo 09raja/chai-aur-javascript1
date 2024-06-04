b981c634359723def7dacd0bba5514cf36049cf2
supriya karmakar <140977487+09raja@users.noreply.github.com>
2024-06-04 10:09:03 +0000

    conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,4 +1,20 @@
-let Score ="67uu"
+let Score ="raja"
 let valueInNumber =Number(Score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+IsLoogedIn =1
+// let isloggedme =Boolean(IsLoogedIn)
+console.log(IsLoogedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "supriya" => true
+ let number =77;
+let nnumber =String(number)
+console.log(nnumber);
+console.log(typeof nnumber);
