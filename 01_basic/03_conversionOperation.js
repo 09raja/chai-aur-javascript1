e4f4e950042349e908f04731e546424301ed489d
supriya karmakar <140977487+09raja@users.noreply.github.com>
2024-06-04 10:38:24 +0000

    last

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let Score ="raja"
 let valueInNumber =Number(Score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -9,12 +9,29 @@ console.log(valueInNumber);
 
 IsLoogedIn =1
 // let isloggedme =Boolean(IsLoogedIn)
-console.log(IsLoogedIn);
+// console.log(IsLoogedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "supriya" => true
  let number =77;
 let nnumber =String(number)
-console.log(nnumber);
-console.log(typeof nnumber);
+// console.log(nnumber);
+// console.log(typeof nnumber);
+
+// *********************** Operations ***********************
+
+let value = 9
+let negvalue = -value
+// console.log(negvalue);
+
+
+let str1 = "hi"
+let str2 = " supriya"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
